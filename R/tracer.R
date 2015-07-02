c41dd776b2214d5fb8389d806710d7f068a0597e
Noam Ross <noam.ross@gmail.com>
2015-07-02 12:04:06 -0700

    Exporting tracer function

--- R/tracer.R
+++ R/tracer.R
@@ -1,10 +1,12 @@
 #' @export
 tracer <- function(x) UseMethod("tracer")
 
+#' @export
 tracer.default <- function(...) {
   stop("Object must be of class 'traced'")
-  }
+}
 
+#' @export
 tracer.traced = function(opt) {
   attr(opt, "trace")
 }
